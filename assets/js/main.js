3e3a8007016625d2298360b8865f9e47a815def2
ctznnn <sikocak334@gmail.com>
2024-11-09 10:35:37 +0700

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -250,3 +250,23 @@ self.addEventListener('fetch', (event) => {
 
 });
 ();
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
